048f8da6ae5fa380a8e48cc79e45572b2de139bc
Ciaran Hayes <hayes.c.hayes@gmail.com>
2025-07-15 01:45:37 +0200

    Count can now tack the exact count on specified times

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,34 +1,48 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 function Counter () {
     const [total, setTotal] = useState(0);
     const [count, setCount] = useState(0);
-    const [hourStart, setHourStart] = useState<number[]>([]);
     const [started, setStarted] = useState(false);
+    const [countOnHour, setCountOnHour] = useState<number | null>(null);
+    const [hourCount, setHourCount] = useState<number[]>([]);
 
-    let now = new Date();
-    let target = new Date();
-    target.setHours(18, 30, 0, 0);
+    let refCount = useRef(0);
 
-    const delay = target.getTime() - now.getTime();
-
-    
     function addVisitors() {
         setCount(count + 1);
         setTotal(total + 1);
+        refCount.current = refCount.current + 1;
     }
 
     function removeVisitors() {
         setCount(count - 1);
+        refCount.current = refCount.current - 1;
     }
 
     function makeStarted() {
         setStarted(true);
+        let now = new Date();
+        let target = new Date();
+        target.setHours(1, 45, 0, 0);
+        const delay = 10000;
+        target.getTime() - now.getTime();
+
+        console.log("Started clicked at:", new Date().toLocaleTimeString());
+        
+        setTimeout(() => {
+            setCountOnHour(refCount.current);
+        }, delay);
     }
 
-    setTimeout(() => {
-        setHourStart([...hourStart, count]);
-    }, delay);
+
+    useEffect(() => {
+        console.log("countOnHour changed:", countOnHour);
+        if (countOnHour !== null) {
+            setHourCount(prev => [...prev, countOnHour]);
+        }
+    }, [countOnHour]);
+    
 
     return (
         <>
@@ -36,8 +50,8 @@ function Counter () {
         <h2>Total Today: {total}</h2>
         <h2>Current guests: {count}</h2>
         <h3></h3>
-        {started === false ? <button onClick={makeStarted}>Start</button> : <div><button onClick={removeVisitors}>-</button> <button onClick={addVisitors}>+</button></div>}
-        {hourStart.map((hourNumberAtStart, index) => <p key={index}>{hourNumberAtStart}</p>)}
+        {started === false ? <button onClick={makeStarted} disabled={started}>Start</button> : <div><button onClick={removeVisitors}>-</button> <button onClick={addVisitors}>+</button></div>}
+        {hourCount.map((hourNumberAtStart, index) => <p key={index}>{hourNumberAtStart}</p>)}
         </>
     )
 }
