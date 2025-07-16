5f68244e7d76c4038a55c4a49d9e50b85e3d6e36
Ciaran Hayes <hayes.c.hayes@gmail.com>
2025-07-16 21:37:21 +0200

    Found possible solution to the timing mechanic for hourly stats

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,92 +1,115 @@
 import { useState, useEffect, useRef } from "react";
 
-function Counter () {
-    const [total, setTotal] = useState(0);
-    const [count, setCount] = useState(0);
-    const [started, setStarted] = useState(false);
-    const [countOnHour, setCountOnHour] = useState<number | null>(null);
-    const [hourCount, setHourCount] = useState<number[]>([]);
-    const [maxCapacity, setMaxCapacity] = useState<number>(0);
-    const [warning, setWarning] = useState("");
-    const [percentageIn, setPercentageIn] = useState<number>(0);
-    const [dangerStyling, setDangerStyling] = useState<React.CSSProperties>({});
-    const [spacesLeft, setSpacesLeft] = useState<number>();
+function Counter() {
+  const [total, setTotal] = useState(0);
+  const [count, setCount] = useState(0);
+  const [started, setStarted] = useState(false);
+  const [countOnHour, setCountOnHour] = useState<number | null>(null);
+  const [hourCount, setHourCount] = useState<number[]>([]);
+  const [maxCapacity, setMaxCapacity] = useState<number>(0);
+  const [warning, setWarning] = useState("");
+  const [percentageIn, setPercentageIn] = useState<number>(0);
+  const [dangerStyling, setDangerStyling] = useState<React.CSSProperties>({});
+  const [spacesLeft, setSpacesLeft] = useState<number>();
 
-    let refCount = useRef(0);
+  let refCount = useRef(0);
 
-    function addVisitors() {
-        setCount(count + 1);
-        setTotal(total + 1);
-        refCount.current = refCount.current + 1;
+  function addVisitors() {
+    setCount(count + 1);
+    setTotal(total + 1);
+    refCount.current = refCount.current + 1;
+  }
+
+  function removeVisitors() {
+    setCount(count - 1);
+    refCount.current = refCount.current - 1;
+  }
+
+  function stopCount() {
+    setCount(count);
+  }
+
+  function makeStarted() {
+    setStarted(true);
+    let now = new Date();
+    let target = new Date();
+    for (let i = 0; i < 10; i++) {
+      target.setHours(21, (35 + i), 0, 0);
+      let delay = target.getTime() - now.getTime();
+      setTimeout(() => {
+      setCountOnHour(refCount.current);
+    }, delay);
+    5;
     }
+    console.log("Started clicked at:", new Date().toLocaleTimeString());
+  }
 
-    function removeVisitors() {
-        setCount(count - 1);
-        refCount.current = refCount.current - 1;
+  useEffect(() => {
+    console.log("countOnHour changed:", countOnHour);
+    if (countOnHour !== null) {
+      setHourCount((prev) => [...prev, countOnHour]);
     }
+  }, [countOnHour]);
 
-    function stopCount() {
-        setCount(count)
+  function handleMaxValue(e: React.ChangeEvent<HTMLInputElement>) {
+    setMaxCapacity(Number(e.target.value));
+  }
+
+  useEffect(() => {
+    setSpacesLeft(maxCapacity - count);
+  }, [count]);
+
+  useEffect(() => {
+    let percentage = Math.floor((count / maxCapacity) * 100);
+    setPercentageIn(percentage);
+    if (percentage >= 100) setWarning("Capacity full");
+    else if (percentage >= 90) {
+      setWarning("You are nearly at capacity");
+      setDangerStyling({ color: "red" });
+    } else if (percentage >= 80) {
+      setWarning("Starting to get full");
+      setDangerStyling({ color: "orange" });
+    } else {
+      setWarning("");
+      setDangerStyling({ color: "black" });
     }
+  }, [count, percentageIn]);
 
-    function makeStarted() {
-        setStarted(true);
-        let now = new Date();
-        let target = new Date();
-        target.setHours(14, 19, 0, 0);
-        const delay = target.getTime() - now.getTime();
-
-        console.log("Started clicked at:", new Date().toLocaleTimeString());
-        
-        setTimeout(() => {
-            setCountOnHour(refCount.current);
-        }, delay);5
-    }
-
-    useEffect(() => {
-        console.log("countOnHour changed:", countOnHour);
-        if (countOnHour !== null) {
-            setHourCount(prev => [...prev, countOnHour]);
-        }
-    }, [countOnHour]);
-
-    function handleMaxValue(e: React.ChangeEvent<HTMLInputElement>) {
-        setMaxCapacity(Number(e.target.value))
-    }
-
-    useEffect(() => {
-        setSpacesLeft(maxCapacity - count);
-    }, [count])
-
-    useEffect(() => {
-        let percentage = Math.floor((count/maxCapacity) * 100);
-        setPercentageIn(percentage)
-        if (percentage >= 100) setWarning("Capacity full");
-        else if (percentage >= 90) {
-            setWarning("You are nearly at capacity");
-            setDangerStyling({color: "red"});
-        }
-        else if (percentage >= 80) {
-            setWarning("Starting to get full");
-            setDangerStyling({color: "orange"});
-        } else {
-            setWarning("");
-            setDangerStyling({color: "white"});
-        }
-    }, [count, percentageIn])
-
-    return (
-        <>
-        <h1>Counter</h1>
-        <h2>Total Today: {total}</h2>
-        <h2 style={dangerStyling}>Current guests: {count}</h2>
-        <h3 style={dangerStyling}>{warning}</h3>
-        <h4>{percentageIn >= 90 && `Spaces left: ${spacesLeft}`}</h4>
-        {started === true && <div><h2>Capacity Percentage: {percentageIn}%</h2><h3>Max Capacity: {maxCapacity}</h3></div>}
-        {started === false ? <div><input type="number" onChange={handleMaxValue} /><button onClick={makeStarted} disabled={started}>Start</button></div>  : <div><button onClick={count > 0 ?  removeVisitors : stopCount}>-</button> <button onClick={count < maxCapacity ? addVisitors : stopCount}>+</button></div>}
-        {hourCount.map((hourNumberAtStart, index) => <p key={index}>{hourNumberAtStart}</p>)}
-        </>
-    )
+  return (
+    <>
+      <h1>Counter</h1>
+      <h4>{percentageIn >= 90 && `Spaces left: ${spacesLeft}`}</h4>
+      {started === true && (
+        <div>
+          <h2>Total Today: {total}</h2>
+          <h2 style={dangerStyling}>Current guests: {count}</h2>
+          <h3 style={dangerStyling}>{warning}</h3>
+          <h2>
+            Capacity Percentage: {percentageIn > 0 ? `${percentageIn}%` : `0%`}
+          </h2>
+          <h3>Max Capacity: {maxCapacity}</h3>
+        </div>
+      )}
+      {started === false ? (
+        <div>
+          <input type="number" onChange={handleMaxValue} />
+          <button onClick={makeStarted} disabled={started}>
+            Start
+          </button>
+        </div>
+      ) : (
+        <div>
+          <button onClick={count > 0 ? removeVisitors : stopCount}>-</button>{" "}
+          <button onClick={count < maxCapacity ? addVisitors : stopCount}>
+            +
+          </button>
+        </div>
+      )}
+      {hourCount.map((hourNumberAtStart, index) => (
+        <p key={index}>{hourNumberAtStart}</p>
+      ))}
+    </>
+  );
 }
 
 export default Counter;
